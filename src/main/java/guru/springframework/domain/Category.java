f3193cf01fa4a939a91c758673cd1b14c6e4a4d5
serbart20 <serbart.dev@gmail.com>
2024-06-04 16:01:50 +0200

    add category many to many relationship

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,46 @@
+package guru.springframework.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return this.recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+}
